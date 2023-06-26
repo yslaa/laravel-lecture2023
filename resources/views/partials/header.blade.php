55d101a7a371431c45c1eed09e464fd3094bb6c0
rommel dalisay <rommelmdalisay@gmail.com>
2023-06-26 13:32:39 +0800

    search form

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -38,8 +38,9 @@
           </a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <form action="{{route('search')}}"  method="POST" class="form-inline my-2 my-lg-0">
+      @csrf
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="term">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
